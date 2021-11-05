2ed685f72056503b346d830d9876bf2b9b7ff620
fardhanardhi <fa.dsg.id@gmail.com>
2021-11-05 08:54:26 +0700

    add meals state & bloc

---
+++ lib/data/blocs/fetch_meals/fetch_meals_state.dart
@@ -0,0 +1,10 @@
+part of 'fetch_meals_cubit.dart';
+
+abstract class FetchMealsState extends Equatable {
+  const FetchMealsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchMealsInitial extends FetchMealsState {}
---
+++ lib/data/repositories/meal_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:meals/data/db/database.dart';
+import 'package:meals/data/models/category.dart';
+
+class CategoryRepository {
+  CategoryRepository(this._client, this._database);
+
+  final Dio _client;
+  final Database _database;
+
+  Future<CategoriesRes> fetchMealsByCategory() async {
+    try {
+      final response = await _client.get('/categories.php');
+      return CategoriesRes.fromJson(response.toString());
+    } on DioError catch (ex) {
+      String errorMessage = json.decode(ex.response.toString())["errorMessage"];
+      throw Exception(errorMessage);
+    }
+  }
+
+  Future persistCategories(List<CategoriesCompanion> entries) async {
+    try {
+      await _database.addCategories(entries);
+    } catch (ex) {
+      throw Exception(ex);
+    }
+  }
+}
